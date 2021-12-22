84667cccec4a7d5f9ca51b6b3949c90ccf6e51e7
Ronin <mdrewj@live.com>
2021-12-22 16:02:52 +0300

    Initial commit

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -7,21 +7,22 @@
             <div class="column is-half">
               <h1 class="title">
                 Starter template for
-                <a href>Nuxt.js</a> with
-                <a href>KeystoneJS</a>.
-                R0N1n-dev
+                <a href>Nuxt.js</a> with <a href>KeystoneJS</a>. R0N1n-dev
+                template
               </h1>
-              <h2
-                class="subtitle"
-              >Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</h2>
+              <h2 class="subtitle">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry.Lorem Ipsum has been the industry's standard dummy text
+                ever since the 1500s.
+              </h2>
               <a href class="button is-primary is-medium">Get Started</a>
             </div>
-            <div class="column">
+            <!--<div class="column">
               <img
                 class="terminal-img"
                 src="https://user-images.githubusercontent.com/7957190/58884002-d15ef100-86df-11e9-8d96-a70b0d383cd4.png"
               >
-            </div>
+            </div>-->
           </dic>
         </div>
       </div>
@@ -29,27 +30,30 @@
     <section>
       <div class="container">
         <div class="columns is-multiline is-mobile is-centered">
-          <div v-for="post in posts" :key="post._id" class="column is-6 is-centered">
+          <div
+            v-for="post in posts"
+            :key="post._id"
+            class="column is-6 is-centered"
+          >
             <div class="header-content">
-              <nuxt-link :to="`/posts/${ post._id }`">
+              <nuxt-link :to="`/posts/${post._id}`">
                 <h3 class="title">{{ post.title }}</h3>
               </nuxt-link>
             </div>
 
             <div class="subheader-content has-text-centered">
               <p>
-                {{ post.publishedDate}}
-                <span
-                  class="tag is-primary"
-                  v-for="cat in post.categories"
-                >{{ cat }}</span>
+                {{ post.publishedDate }}
+                <span class="tag is-primary" v-for="cat in post.categories">{{
+                  cat
+                }}</span>
               </p>
-              <hr>
+              <hr />
             </div>
 
             <div class="single-content">{{ post.content.brief }}</div>
             <div class="has-text-centered">
-              <nuxt-link :to="`/posts/${ post._id }`">Read More</nuxt-link>
+              <nuxt-link :to="`/posts/${post._id}`">Read More</nuxt-link>
             </div>
           </div>
         </div>
@@ -72,7 +76,7 @@
 <script>
 import axios from "axios";
 
-const debug = data => {
+const debug = (data) => {
   console.log("debug", data);
   return data;
 };
@@ -81,12 +85,12 @@ export default {
   asyncData({ params }) {
     return axios
       .get(`${process.env.KEYSTONE_URL}/api/v1/posts`)
-      .then(res => res.data)
+      .then((res) => res.data)
       .then(debug)
-      .catch(reason => {
+      .catch((reason) => {
         console.error(reason);
         return {};
       });
-  }
+  },
 };
 </script>
